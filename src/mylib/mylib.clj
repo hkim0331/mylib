d17c09536b75b4a81054f22e4cb08f910e9d6249
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-02-29 11:03:44 +0900

    added to-base function

--- src/mylib/mylib.clj
+++ src/mylib/mylib.clj
@@ -1,6 +1,27 @@
 (ns mylib.mylib)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+;; (defn foo
+;;   "I don't do a whole lot."
+;;   [x]
+;;   (prn x "Hello, World!"))
+
+(defn to-base-base-aux [b n ret]
+  (if (zero? n)
+    ret
+    (recur b (quot n b) (cons (mod n b) ret))))
+
+(defn to-base
+  "convert an integer `n` into a base `base` list, return it.
+   example,
+   (to-base 2 144) => (1 0 0 1 0 0 0 0)"
+  ([base]
+   (fn [n] (to-base base n)))
+  ([base n]
+   (if (zero? n)
+     [0]
+     (to-base-base-aux base n []))))
+
+(comment
+  ((to-base 2) 345)
+  (map (to-base 2) (range 10))
+  :rcf)
