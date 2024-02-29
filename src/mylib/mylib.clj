d1ef905e867a369876b469b0e573381335b2f247
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-02-29 10:58:47 +0900

    first try clojure `private` library

---
+++ src/mylib/mylib.clj
@@ -0,0 +1,6 @@
+(ns mylib.mylib)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/mylib/mylib_test.clj
@@ -0,0 +1,7 @@
+(ns mylib.mylib-test
+  (:require [clojure.test :refer :all]
+            [mylib.mylib :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
